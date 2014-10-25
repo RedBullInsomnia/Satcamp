c1d259ee02a839a841bd7bfd36a52218b6492231
RedBullInsomnia <mr.huwok@gmail.com>
2014-10-25 12:46:46 +0200

    Fuck

--- SatelliteClient/Window.cs
+++ SatelliteClient/Window.cs
@@ -17,72 +17,72 @@ namespace SatelliteClient
     public partial class Window : Form
     {
         Bitmap image;
-        bool _bConnected;
-        System.Timers.Timer _captureTimer;
-        System.Timers.Timer _updateTimer;
+        bool _bConnected, connectedFlag, saveFlag;
+        System.Timers.Timer captureTimer;
+        System.Timers.Timer updateTimer;
         ChannelFactory<SatelliteServer.ISatService> _scf;
-        SatelliteServer.ISatService _satService;
+        SatelliteServer.ISatService satService;
 
         public Window()
         {
             InitializeComponent();
-            _captureTimer = new System.Timers.Timer(250);
-            _updateTimer = new System.Timers.Timer(50);
-            _updateTimer.Elapsed += _updateTimer_Elapsed;
-            _captureTimer.Elapsed += _captureTimer_Elapsed;
+            captureTimer = new System.Timers.Timer(250);
+            updateTimer = new System.Timers.Timer(50);
+            updateTimer.Elapsed += _updateTimer_Elapsed;
+            captureTimer.Elapsed += _captureTimer_Elapsed;
             _bConnected = false;
         }
 
         void _captureTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            _captureTimer.Enabled = false;
+            captureTimer.Enabled = false;
             if (_bConnected)
             {
-                this.Invoke(new Action(() =>
-                {
+                this.Invoke(new Action(() => acquireFrame()));
+                /*{
                     captureBn.Enabled = false;
-                    byte[] buffer = _satService.Capture();
-                    Console.Write("Received image with " + buffer.Length + " bytes.");
+                    byte[] buffer = satService.Capture();
+                    //Console.Write("Received image with " + buffer.Length + " bytes.");
                     MemoryStream stream = new MemoryStream(buffer);
                     pictureBox.Image = new Bitmap(stream);
                     image = (Bitmap)pictureBox.Image;
                     captureBn.Enabled = true;
-                }));
+                }));*/
             }
-            _captureTimer.Enabled = true;
+            captureTimer.Enabled = true;
         }
 
         void _updateTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            _updateTimer.Enabled = false;
+            updateTimer.Enabled = false;
             this.Invoke(new Action(() =>
             {
                 if (_bConnected)
                 {
-                    double[] euler = _satService.GetEulerAngles();
+                    double[] euler = satService.GetEulerAngles();
                     tbRoll.Text = euler[0].ToString();
                     tbPitch.Text = euler[1].ToString();
                     tbYaw.Text = euler[2].ToString();
 
-                    pitchTrackBar.Value = _satService.GetServoPos(0);
-                    yawTrackBar.Value = _satService.GetServoPos(1);
+                    pitchTrackBar.Value = satService.GetServoPos(0);
+                    yawTrackBar.Value = satService.GetServoPos(1);
                 }
             }));
-            _updateTimer.Enabled = true;
+            updateTimer.Enabled = true;
         }
 
         private void captureBn_Click(object sender, EventArgs e)
         {
-            _captureTimer.Enabled = false;
+            captureTimer.Enabled = false;
             if (_bConnected)
             {
                 captureBn.Enabled = false;
-                byte[] buffer = _satService.Capture();
-                Console.Write("Received image with " + buffer.Length + " bytes.");
+                byte[] buffer = satService.Capture();
+                // Console.Write("Received image with " + buffer.Length + " bytes.");
                 using (MemoryStream stream = new MemoryStream(buffer))
                 {
                   pictureBox.Image = Bitmap.FromStream(stream);
-                  image = (Bitmap)pictureBox.Image;
+                  //image = (Bitmap)pictureBox.Image;
                   captureBn.Enabled = true;
                 }
             }
@@ -94,7 +94,7 @@ namespace SatelliteClient
 
         private void Window_Load(object sender, EventArgs e)
         {
-            _updateTimer.Start();
+            updateTimer.Start();
             ipTb.Text = Settings.Default["IP"].ToString();
         }
 
@@ -114,7 +114,7 @@ namespace SatelliteClient
                             "net.tcp://" + ipTb.Text + ":8000");
                 //"net.tcp://192.168.1.137:8000");
 
-                _satService = _scf.CreateChannel();
+                satService = _scf.CreateChannel();
             }
             catch (Exception ex)
             {
@@ -130,7 +130,7 @@ namespace SatelliteClient
 
         private void Window_FormClosing(object sender, FormClosingEventArgs e)
         {
-            _updateTimer.Stop();
+            updateTimer.Stop();
             Settings.Default.Save();
         }
 
@@ -140,7 +140,7 @@ namespace SatelliteClient
             {
                 this.BeginInvoke(new Action(() =>
                 {
-                    _satService.SetStabilization(stabilizeCb.Checked);
+                    satService.SetStabilization(stabilizeCb.Checked);
                 }));
             }
         }
@@ -151,7 +151,7 @@ namespace SatelliteClient
             {
                 this.BeginInvoke(new Action(() =>
                 {
-                    _satService.SetServoPos(0, pitchTrackBar.Value);
+                    satService.SetServoPos(0, pitchTrackBar.Value);
                 }));
             }
         }
@@ -162,23 +162,109 @@ namespace SatelliteClient
             {
                 this.BeginInvoke(new Action(() =>
                 {
-                    _satService.SetServoPos(1, yawTrackBar.Value);
+                    satService.SetServoPos(1, yawTrackBar.Value);
                 }));
             }
         }
 
         private void videoBn_Click(object sender, EventArgs e)
         {
-            _captureTimer.Enabled = true;
+            captureTimer.Enabled = true;
         }
 
         private void saveImageButton_Click(object sender, EventArgs e)
         {
           if(image != null){
-			String picture_name = "satpic_" + DateTime.Now.ToString("yyyymmdd_HHmmss");
-			  image.Save("c:\\Users\\Satcamp1\\"+ picture_name + ".png", System.Drawing.Imaging.ImageFormat.Png);
-		  }else
-			  MessageBox.Show("You failed");
+            lock (image)
+            {
+              String picture_name = "satpic_" + DateTime.Now.ToString("yyyymmdd_HHmmss");
+              image.Save("c:\\Users\\Satcamp1\\" + picture_name + ".png", System.Drawing.Imaging.ImageFormat.Png);
+            }
+          } else
+			      MessageBox.Show("You failed");
+        }
+
+        /**
+           * Acquire a from from the remote server
+           */
+        private void acquireFrame()
+        {
+          try
+          {
+            byte[] buffer = satService.Capture();
+            Console.WriteLine("Received image with " + buffer.Length + " bytes.");
+            MemoryStream stream = new MemoryStream(buffer);
+
+            lock (image)
+            {
+              image = (Bitmap)Bitmap.FromStream(stream);
+
+              if (saveFlag)
+              {
+                saveImage(image);
+                saveFlag = false;
+              }
+
+              pictureBox.Image = image;
+            }
+          }
+          catch (System.ServiceModel.CommunicationException ex)
+          {
+            disconnect(ex.Message);
+          }
+          catch (System.TimeoutException ex)
+          {
+            disconnect(ex.Message);
+          }
+          catch (Exception ex)
+          {
+            disconnect("Ex capture : ### : " + ex.Message);
+          }
+        }
+
+        /**
+           * Save the image currently saved (image class variable) (must be different from null)
+           * The accesses to the image must be locked
+           */
+        private void saveImage(Bitmap img)
+        {
+          String picture_name = "satpic_" + DateTime.Now.ToString("yyyymmdd_HHmmss");
+          image.Save("D:\\Documents\\BitBucket\\Satcamp\\" + picture_name + ".png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        /**
+         * Disconnect the client (stop timers and close the channel factory) and 
+         * display a message in a message box
+         */
+        private void disconnect(String message)
+        {
+          MessageBox.Show("Client disconnected : " + message);
+
+          disconnect();
+        }
+
+        /**
+         * Disconnect the client : stop timers and close the channel factory
+         */
+        private void disconnect()
+        {
+          connectedFlag = false;
+          saveFlag = false;
+
+         // Console.WriteLine("Mode switched to NONE");
+          //mode = Mode.NONE;
+
+          image = null;
+
+          // stop timers
+          updateTimer.Enabled = false;
+          captureTimer.Enabled = false;
+
+          //if (scf != null)
+           // scf.Abort();
+
+          // update button status
+          //setButtonsDisconnected();
         }
     }
 }
